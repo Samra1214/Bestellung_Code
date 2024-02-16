4cc599ff0515dcd2358609d8fd8121f248f1fffb
Samra1214 <160223529+Samra1214@users.noreply.github.com>
2024-02-16 15:04:16 +0000

    Fertig

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -2,25 +2,79 @@ package com.cc.java;
 
 public class App {
     
+    public static void main(String[] args) 
+    {
+        
+        // Variablen deklarieren
 
-    public static void main(String[] args) {
-        //Variablen deklarieren
         String food = "noch kein Burger gewählt";
         String drink = "noch kein Getränk gewählt";
         String itemNumStr;
-        String Flag;
+        String flag;
 
-       
+        System.out.println(food);  
+        System.out.println(drink); 
 
-    
-        System.out.printIn(food);
-        System.out.printIn(drink);
+        // Bestelldialog 1 : Ausgabe
+        System.out.println("-------------------------------");
+        System.out.println("Guten Tag bei MD, Ihre Bestellung bitte!");
+        System.out.println("Hamburger(1), Cheeseburger(2) oder Chilliburger(3) ?");
 
-    }    
+        // Bestelldialog 1 : Eingabe
+        itemNumStr = System.console().readLine("Ihre Wahl? : ");
+        System.out.println(itemNumStr);  // 1 oder 2 oder 3
 
+        // Zuordnung Zahl --> Burger
+        switch (itemNumStr)
+        {
+            case "1":
+                food = "Hamburger";
+                break;
+            case "2":
+                food = "Cheeseburger";
+                break;
+            case "3":
+                food = "Chilliburger";
+                break;
+        }
+
+        System.out.println(food);
+
+
+        // Entscheidung Getränk dazu?
+        System.out.println("Möchten Sie ein Getränk dazu bestellen? (j/n)");
+        flag = System.console().readLine("(j/n)? :");
+
+        System.out.println(flag);
+
+      	 // Getränkeasuwahl
+        if (flag.equals("j")) {
+
+            System.out.println("Cola(1) , Fanta(2), Sprite(3)?");
+           itemNumStr = System.console().readLine("Ihre Wahl? : ");
+
+            switch (itemNumStr)
+            {
+                case "1":
+                    drink = "Cola" ;
+                    break;
+                case "2":
+                    drink = "Fanta";
+                    break;
+                case "3":
+                    drink = "Sprite" ;
+                    break;
+            }
+
+            System.out.println("Hier ist Ihre Bestellung: " +  food + " und " +  drink);
+
+        } else{
+            System.out.println("Hier ist Ihre Bestellung: " +  food);
+        }
+
+        System.out.println("Vielen Dank, und Auf Wiedersehen!");
+
+
+    }
 
 }
-
-
-
-
