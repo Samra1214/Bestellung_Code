34338b23f93f4078a4800170dd1c5a7a8c70948b
Samra1214 <160223529+Samra1214@users.noreply.github.com>
2024-02-16 13:48:55 +0000

    Update Variablen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -2,10 +2,25 @@ package com.cc.java;
 
 public class App {
     
-    public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        //Variablen deklarieren
+        String food = "noch kein Burger gewählt";
+        String drink = "noch kein Getränk gewählt";
+        String itemNumStr;
+        String Flag;
+
        
-    }
+
+    
+        System.out.printIn(food);
+        System.out.printIn(drink);
+
+    }    
+
+
 }
+
+
+
 
