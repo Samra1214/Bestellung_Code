eb297b3a0d749bcdee543a19409cba990082b326
Samra1214 <160223529+Samra1214@users.noreply.github.com>
2024-02-20 14:57:09 +0000

    neu

--- com/cc/java/ArrayPlayground.java
+++ com/cc/java/ArrayPlayground.java
@@ -3,7 +3,7 @@ package com.cc.java;
 
 public class ArrayPlayground {
 
-	static int [] numArr = {2,4,6,7,20};
+	static int [] numArr = {2,4,6,7,20,3,6,6,7,89};
 	// static String strArr = {"Ich", "bin", "Maxine", "Mützerich"};
 
     public static void main(String[] args) {
@@ -29,12 +29,33 @@ public class ArrayPlayground {
 
 
 		System.out.println("---------------------------------------");
-		System.out.print(numArr[0] + ",");
-		System.out.print(numArr[1] + ",");
-		System.out.print(numArr[2] + ",");
-		System.out.print(numArr[3] + ",");
-		System.out.println(numArr[4] + ",");
-		System.out.println("---------------------------------------");
+
+		//System.out.print(numArr[0] + ",");
+		//System.out.print(numArr[1] + ",");
+		//System.out.print(numArr[2] + ",");
+		//System.out.print(numArr[3] + ",");
+		//System.out.println(numArr[4] + ",");
+		
+
+	for (int i = 0; i < numArr.length; i++) {
+		if (i < numArr.length -1) {
+			System.err.print(numArr[i] + ",");
+			
+		} else {
+			System.err.print(numArr[i]);
+			
+		}
+
+
+
+
 
 	}
-	}
+
+
+	System.out.println("---------------------");
+
+}
+
+}
+
