99ee184f82ee033dd7518eeb704db589d87c0016
Samra1214 <160223529+Samra1214@users.noreply.github.com>
2024-02-20 12:08:05 +0000

    Array.Javaapp

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,5 +1,5 @@
 package com.cc.java;
-
+  
 public class App {
     
     public static void main(String[] args) 
---
+++ com/cc/java/ArrayPlayground.java
@@ -0,0 +1,40 @@
+package com.cc.java;
+
+
+public class ArrayPlayground {
+
+	static int [] numArr = {2,4,6,7,20};
+	// static String strArr = {"Ich", "bin", "Maxine", "Mützerich"};
+
+    public static void main(String[] args) {
+
+		// System.out.ptintln(numArr);
+
+		outputArr();
+
+		numArr[0] = 100;
+		
+
+		System.err.println("Elemente:" + numArr.length);
+		// numArr[3] = 101;
+		numArr[numArr.length-1] = 101;
+
+
+		outputArr();
+	}
+
+
+	static void outputArr(){
+
+
+
+		System.out.println("---------------------------------------");
+		System.out.print(numArr[0] + ",");
+		System.out.print(numArr[1] + ",");
+		System.out.print(numArr[2] + ",");
+		System.out.print(numArr[3] + ",");
+		System.out.println(numArr[4] + ",");
+		System.out.println("---------------------------------------");
+
+	}
+	}
