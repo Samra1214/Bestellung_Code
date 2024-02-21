812acd07f905297e04c2e4da55ccebbb9b89bf28
Samra1214 <160223529+Samra1214@users.noreply.github.com>
2024-02-21 21:50:24 +0000

    neuuuu

--- com/cc/java/BubbleSort.java
+++ com/cc/java/BubbleSort.java
@@ -3,7 +3,7 @@ package com.cc.java;
 
 public class BubbleSort {
  
-    static int [] numArr = {3, 1, 5, 50, 34, 6 }; // zu sortierendes Array
+    static int [] numArr = {5,8,33,47,99,12 }; // zu sortierendes Array
 
     public static void main(String [] args){
     
@@ -17,7 +17,7 @@ public class BubbleSort {
     
             for(i = 0; i < numArr.length; i ++){  // gesamtes Array
                 
-                for(j = 0; j < (numArr.length - 1 ); j++)
+                for(j = 0; j < (numArr.length -( i + 1 )); j++)
                 { // Sucharray
 
                      swapped = false; // Annahme: kein Tausch erfolgt
